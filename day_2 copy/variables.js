a9838b883fb4b45cd113e4496e30688d3ef283e1
thamaragerigr <thamaragerigr@gmail.com>
2020-03-03 21:50:19 +0100

    day 2

--- day_2 copy/variables.js
+++
@@ -1,5 +0,0 @@
-//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
-let string = 'string';
-let boolean = true;
-let undefinedVariable = undefined;
-let nullVariable = null;
