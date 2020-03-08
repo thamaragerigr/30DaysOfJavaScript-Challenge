c4fcf15178733095fea7f080de75f1cfdc067549
thamaragerigr <thamaragerigr@gmail.com>
2020-03-08 23:49:14 +0100

    day 6-7

--- day_6/main.js
+++ day_6/main.js
@@ -29,8 +29,72 @@ const countries = [
 //   console.log(i);
 // }
 
-let i = 0;
-while (i <= 10){
-    console.log(i);
-    i++;
+// let i = 0;
+// while (i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+
+//Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+//   }
+
+  let i = 10;
+// while (i >= 0){
+//     console.log(i);
+//     i--;
+// }
+
+// do {
+//   console.log(i);
+//   i--;
+// } while (i >= 0);
+
+// Iterate 0 to n using for loop
+
+// for (let i = 0; i < 20; i++) {
+//     console.log(0);
+//  }
+
+// Write a loop that makes the following pattern using console.log():
+
+
+
+for (let i = 1; i < 7; i++) {
+  let strng = ' ';
+  for (let j = 1; j <= i; j++) {
+    strng += '#';
+  }
+  console.log(strng)
 }
+
+//Use loop to print the following pattern:
+
+//Using loop print the following pattern
+
+//Use for loop to iterate from 0 to 100 and print only even numbers
+
+//Use for loop to iterate from 0 to 100 and print only odd numbers
+
+//Use for loop to iterate from 0 to 100 and print only prime numbers
+
+// Use for loop to iterate from 0 to 100 and print and print the sum of all numbers.
+
+//Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+
+//Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
+
+//Develop a small script which generate array of 5 random numbers
+
+//Develop a small script which generate array of 5 random numbers and the numbers must be unique
+
+//Develop a small script which generate a six characters random id:
+
