6e43bc6eebefb34788339a295eca02c668c9b6d2
thamaragerigr <thamaragerigr@gmail.com>
2020-03-08 22:58:32 +0100

    day 6-7

--- day_5/main.js
+++ day_5/main.js
@@ -22,7 +22,7 @@ const mixedDataTypes = [ 12, 'hi', true, [1, 2, 3]];
 
 //Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
 const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'];
- 
+
 //Print the array using console.log()
 console.log(itCompanies);
 
@@ -109,6 +109,79 @@ itCompanies.splice(0,itCompanies.length);
 console.log(itCompanies);
 
 
+//First remove all the functions and change the string to array and count the number of words in the array
+let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.split(' ');
+console.log(words);
+console.log(words.length);
 
-console.log(frontEnd);
-console.log(backEnd);
+//In the following shopping cart add, remove, edit items
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+shoppingCart.unshift('Meat');
+console.log(shoppingCart);
+
+//add sugar at the end of you shopping cart if it has not been already added
+shoppingCart.push('Sugar');
+console.log(shoppingCart);
+//modify tea to 'Green Tea'
+shoppingCart[3] = 'Green Tea';
+console.log(shoppingCart);
+
+
+//In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+for(let i = 0; i < countries.length; i++){
+    if(countries[i] === 'Ethiopia') {
+        console.log('yeih');
+    }
+};
+
+//In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+    if(frontEnd.includes('SASS')) {
+        console.log('Sass is a CSS preprocess');
+    } else {
+        frontEnd.push('SASS')
+        console.log(frontEnd);
+    };
+
+ //Concatenate the following two variables and store it in a fullStack variable.
+fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
+
+
+
+//Exercise: Level 3
+
+//The following is an array of 10 students ages:
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+// Sort the array and find the min and max age
+//can give errors
+// console.log(ages.sort());
+// console.log(Math.min.apply(null, ages));
+// console.log(Math.max.apply(null, ages));
+
+let min = ages[0];
+let max = ages[ages.length -1];
+console.log(max);
+
+console.log(ages.reduce((min, val) => val < min ? val : min, ages[0]));
+
+// Find the median age(one middle item or two middle items divided by two)
+
+
+// Find the average age(all items divided by number of items)
+
+
+// Find the range of the ages(max minus min)
+
+
+// Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+
+
+//Find the middle country(ies) in the countries array
+
+
+
+//Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
+
---
+++ day_6/main.js
@@ -0,0 +1,36 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+  //Iterate 0 to 10 using for loop, do the same using while and do while loop
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+let i = 0;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
